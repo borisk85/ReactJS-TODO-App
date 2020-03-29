3e8cea28c9bf286656b00ee6847f6e16c05f5821
Boris Komarov <bkomarov85@gmail.com>
2020-03-29 18:33:49 +0600

    Add files

---
+++ todo-app/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import TodoList from './todo/todolist'
+import Context from './context'
+import AddTodo from './todo/addtodo'
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    //  { id: 1, completed: false, title: 'Buy milk' },
+    //  { id: 2, completed: false, title: 'Buy bread ' },
+    //  { id: 3, completed: false, title: 'Buy honey' }
+  ])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    )
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className='wrapper'>
+        <h1>TODO list</h1>
+        <AddTodo onCreate={addTodo} />
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : (
+          <p>No todos!</p>
+        )}
+      </div>
+    </Context.Provider>
+  )
+}
+
+export default App
